876f2f0654e19b93f30023a2034ba022579bed21
taroosg <root@taroosg.dev>
2022-01-19 17:54:07 +0900

    update deploy

--- mod.ts
+++ mod.ts
@@ -1,33 +1,35 @@
-import { Bot } from './deps.ts';
-import init, { fib } from "./pkg/rust_calculate_bot.js";
-// import { serve } from "https://deno.land/std@0.114.0/http/server.ts";
+// import { Bot } from './deps.ts';
+// import init, { fib } from "./pkg/rust_calculate_bot.js";
+// // import { serve } from "https://deno.land/std@0.114.0/http/server.ts";
 
-if (Deno.env.get("ENVIRONMENT") === "production") {
-  const res = await fetch(
-    "https://raw.githubusercontent.com/taroosg/rust-calculate-bot/main/pkg/rust_calculate_bot_bg.wasm"
-  );
-  await init(await res.arrayBuffer());
-} else {
-  await init(Deno.readFile("./pkg/rust_calculate_bot_bg.wasm"));
-}
+// if (Deno.env.get("ENVIRONMENT") === "production") {
+//   const res = await fetch(
+//     "https://raw.githubusercontent.com/taroosg/rust-calculate-bot/main/pkg/rust_calculate_bot_bg.wasm"
+//   );
+//   await init(await res.arrayBuffer());
+// } else {
+//   await init(Deno.readFile("./pkg/rust_calculate_bot_bg.wasm"));
+// }
 
 
 
-const token = Deno.env.get("BOT_TOKEN") as string;
+// const token = Deno.env.get("BOT_TOKEN") as string;
 
-console.log(token);
+// console.log(token);
 
-const bot = new Bot(token);
+// const bot = new Bot(token);
 
-bot.on('text', async (ctx) => {
-  const text = ctx.message?.text;
-  if (text === '/hoge') {
-    console.log(text);
-    const res =  fib(Number(10)).toString() ;
+// bot.on('text', async (ctx) => {
+//   const text = ctx.message?.text;
+//   if (text === '/hoge') {
+//     console.log(text);
+//     const res =  fib(Number(10)).toString() ;
 
-    await ctx.reply(res);
-  }
-})
+//     await ctx.reply(res);
+//   }
+// })
 
-bot.launch();
+// bot.launch();
 
+import { serve } from "https://deno.land/std/http/server.ts";
+serve((req) => new Response("Hello world"));

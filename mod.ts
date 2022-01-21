692732ec9e205a4486341cd2fa131e92edbcaa40
taroosg <root@taroosg.dev>
2022-01-21 23:09:30 +0900

    update grammy

--- mod.ts
+++ mod.ts
@@ -1,37 +1,20 @@
-import { Bot } from './deps.ts';
-import init, { fib, prime_factorization } from "./pkg/rust_calculate_bot.js";
+import { Bot, webhookCallback } from "https://deno.land/x/grammy/mod.ts";
+// You might modify this to the correct way to import your `Bot` object.
+import bot from "./bot.ts";
 
-// if (Deno.env.get("ENVIRONMENT") === "production") {
-//   const res = await fetch(
-//     "https://raw.githubusercontent.com/taroosg/rust-calculate-bot/main/pkg/rust_calculate_bot_bg.wasm"
-//   );
-//   await init(await res.arrayBuffer());
-// } else {
-//   await init(Deno.readFile("./pkg/rust_calculate_bot_bg.wasm"));
-// }
+import { serve } from "https://deno.land/std/http/server.ts";
 
-await init(Deno.readFile("./pkg/rust_calculate_bot_bg.wasm"));
+const handleUpdate = webhookCallback(bot, "std/http");
 
-// 数値かどうかをチェックする関数
-const isNumber = (str:string):boolean => (new RegExp(/^[0-9]+$/)).test(str);
+serve(async (req) => {
+  if (req.method == "POST") {
+    try {
+      return await handleUpdate(req);
+    } catch (err) {
+      console.error(err);
+      return new Response();
+    }
+  }
 
-// レスポンスを作成する関数
-const createResponse = (str: string | undefined ): string =>
-  !str
-    ? 'NaN'
-    : !isNumber(str)
-      ? 'NaN'
-      : !(Number(str) > 0)
-        ? '0'
-        : prime_factorization(Number(str)).join('\n');
-
-const token = Deno.env.get("BOT_TOKEN") as string;
-
-const bot = new Bot(token);
-
-bot.on('text', async (ctx) => {
-  await ctx.reply(createResponse(ctx.message?.text));
-})
-
-bot.launch();
-
+  return new Response();
+});
